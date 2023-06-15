ace3ab165ad4a476f5769cdc526c7bbab535ba5d
Tyler Aycock <tyler.joseph.aycock@gmail.com>
2023-06-15 11:05:07 -0700

    completed lab

---
+++ src/components/Form.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import RobotList from './RobotList'
+
+const Form = (props) => {
+
+    return (
+        <form  onSubmit={props.handleRobotAdd}>
+            <p>What is your new robot's name?</p>
+            <input 
+                type='text' 
+                placeholder='name'
+                value={props.input} 
+                onChange={props.handleInput}/>
+            <button>Create Robot</button>
+        </form>
+    )
+}
+
+export default Form
